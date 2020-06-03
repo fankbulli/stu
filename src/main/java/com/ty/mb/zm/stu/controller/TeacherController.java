9ec4bcc572fc3b3a1bd21c0c426c708f562dd2b5
fankbulli <18000252855@163.com>
2020-06-03 09:54:30 +0800

    教师表

--- src/main/java/com/ty/mb/zm/stu/controller/TeacherController.java
+++ src/main/java/com/ty/mb/zm/stu/controller/TeacherController.java
@@ -1,9 +1,17 @@
 package com.ty.mb.zm.stu.controller;
 
 
+import com.ty.mb.zm.stu.entity.Teacher;
+import com.ty.mb.zm.stu.service.TeacherService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
+    @Autowired
+    private TeacherService teacherService;
 
+    @ApiOperation(value = "教师列表",tags = "教师表")
+    @GetMapping("/list")
+    public List<Teacher> queryList(){
+        List<Teacher> list=teacherService.list();
+        return list;
+    }
+
+    @ApiOperation(value = "根据sno查找教师",tags = "教师表")
+    @GetMapping("/findBySno/{sno}")
+    public Teacher findBySno(@PathVariable Integer sno){
+        Teacher tea=teacherService.getById(sno);
+        return tea;
+    }
 }
 
