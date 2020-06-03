9c3c694adddfaba07b1abc97b0593329bb725ac8
fankbulli <18000252855@163.com>
2020-06-03 14:56:49 +0800

    aop

--- src/main/java/com/ty/mb/zm/stu/aop/AspectAop.java
+++ src/main/java/com/ty/mb/zm/stu/aop/AspectAop.java
@@ -1,7 +1,6 @@
 package com.ty.mb.zm.stu.aop;
 
 import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.extension.api.R;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -18,33 +17,35 @@ import java.util.Map;
 @Aspect
 @Component
 public class AspectAop {
-    private static Logger log=LoggerFactory.getLogger(AspectAop.class);
+    private static Logger log = LoggerFactory.getLogger(AspectAop.class);
 
     @Pointcut("execution (* com.ty.mb.zm.stu.controller..*.*(..))")
-    public void logaop(){}
-    @Around("logaop()")
-    public Object doAround(ProceedingJoinPoint pjp) throws Throwable{
-        Object result=null;
-        try {
-            long start=System.currentTimeMillis();
-            log.info("================================进入方法==================================");
-            MethodSignature methodSignature=(MethodSignature) pjp.getSignature();
+    public void logaop() {
+    }
 
-            Method method=methodSignature.getMethod();
+    @Around("logaop()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        Object result = null;
+        try {
+            long start = System.currentTimeMillis();
+            log.info("================================进入方法==================================");
+            MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+
+            Method method = methodSignature.getMethod();
             log.info("类名称-->{}", JSONObject.toJSONString(method.getDeclaringClass().getCanonicalName()));
-            Object[] args=pjp.getArgs();
-            String[] params= methodSignature.getParameterNames();
-            Map<String,Object> map=new HashMap<>();
-            for(int i=0;i<params.length;i++){
-                map.put(params[i],args[i]);
+            Object[] args = pjp.getArgs();
+            String[] params = methodSignature.getParameterNames();
+            Map<String, Object> map = new HashMap<>();
+            for (int i = 0; i < params.length; i++) {
+                map.put(params[i], args[i]);
             }
             log.info("方法名称-->{}", JSONObject.toJSONString(method.getName()));
             log.info("方法入参-->{}", JSONObject.toJSONString(map));
-            result =pjp.proceed(args);
-            log.info("方法返回-->{}",JSONObject.toJSONString(result));
-            long end=System.currentTimeMillis();
-            log.info("耗时：-->{}ms",end-start);
-        }catch (Throwable e){
+            result = pjp.proceed(args);
+            log.info("方法返回-->{}", JSONObject.toJSONString(result));
+            long end = System.currentTimeMillis();
+            log.info("耗时：-->{}ms", end - start);
+        } catch (Throwable e) {
             log.debug("================================出现异常==================================");
             log.debug(e.getMessage());
         }
